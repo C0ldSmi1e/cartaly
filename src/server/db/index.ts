f79984d657abba86583af4f65fbff16fcf7f0f44
C0ldSmi1e <DanielYu3790@gmail.com>
2026-08-15 12:01:42 -0700

    refactor: use up to date command

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -10,9 +10,9 @@ import * as schema from "@/src/server/db/schema";
 mkdirSync(dirname(env.DATABASE_PATH), { recursive: true });
 
 const sqlite = new Database(env.DATABASE_PATH, { create: true });
-sqlite.exec("PRAGMA journal_mode = WAL;");
-sqlite.exec("PRAGMA busy_timeout = 5000;");
-sqlite.exec("PRAGMA foreign_keys = ON;"); // isForeignKeyViolation relies on this
+sqlite.run("PRAGMA journal_mode = WAL;");
+sqlite.run("PRAGMA busy_timeout = 5000;");
+sqlite.run("PRAGMA foreign_keys = ON;"); // isForeignKeyViolation relies on this
 
 const db = drizzle(sqlite, { schema });
 
